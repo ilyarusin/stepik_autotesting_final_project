380a334fcc2cd1549ebd2f81551f7ac152592abf
Ilya Piminov <ilyapiminov@gmail.com>
2024-08-23 13:40:57 +0500

    добавлен тест кейс добавления товара в корзину, создан класс для страницы товара, отредактированы файлы locators, base_page, main_page

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    VIEW_CART_BUTTON = (By.CSS_SELECTOR, "a[href='/ru/basket/']")
+    PRODUCT_NAME_IN_THE_CART = (By.CSS_SELECTOR, "div.alertinner strong")
+    PRODUCT_NAME_ON_THE_PAGE = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRODUCT_PRICE_IN_THE_CART = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    PRODUCT_PRICE_ON_THE_PRODUCT_PAGE = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_product_to_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_to_cart_button.click()
+
+    def product_name_check(self):
+        product_name_on_the_page = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ON_THE_PAGE).text
+        product_name_in_the_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_THE_CART).text
+        assert product_name_in_the_cart == product_name_on_the_page, f"Название должно быть {product_name_on_the_page}"
+        print("Товар добавлен в корзину")
+    
+    def cart_summ_check(self):
+        product_price_in_the_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_THE_CART).text
+        product_price_on_the_product_page = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_ON_THE_PRODUCT_PAGE).text
+        assert product_price_in_the_cart == product_price_on_the_product_page, "Стоимость корзины не совпадает с ценой товара"
+        print("Сумма в корзине соответствует цене товара")
