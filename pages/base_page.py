59f5c5ff4ade3c7e8adbf5f50f526b8e0be64d5d
Ilya Piminov <ilyapiminov@gmail.com>
2024-08-26 20:43:11 +0500

    Фикс теста test_guest_can_go_to_login_page_from_product_page, фикс локатора login_link в BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -14,7 +14,7 @@ class BasePage():
         self.browser.implicitly_wait(timeout)
 
     def go_to_login_page(self):
-        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
     def go_to_basket_page(self):
--- test_product_page.py
+++ test_product_page.py
@@ -5,18 +5,11 @@ import time
 import pytest
 
 # Тест промо-акций
-@pytest.mark.need_review
+
 @pytest.mark.parametrize('link', 
-["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="AssertionError: Название должно быть Coders at Work")),
-"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+[pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail(reason="AssertionError: Название должно быть Coders at Work")),
 "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
         page = ProductPage(browser, link)
         page.open()
@@ -60,7 +53,6 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-@pytest.mark.xfail(reason="Нужно пофиксить селектор линка")
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
